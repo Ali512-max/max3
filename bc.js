622e20004b2092a2742321e8a917eb80be5c13d8
Ali512-max <53065815+Ali512-max@users.noreply.github.com>
2019-07-23 01:24:31 +0300

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,32 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "!";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'everyone').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : 【DZL】 𝑽 𝑰 𝑹 𝑼 𝑺#5424`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : 【DZL】 𝑽 𝑰 𝑹 𝑼 𝑺#5424 ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Welcome To Communiti Iraq`,"http://twitch.tv/S-F")
+client.user.setStatus("idle")
+});
+
+
+
+client.login("NjAwODExOTQ5MzkwOTU0NTAx.XS5dnw.mwHh9QpSDoFiXwbpdFDzgn01Xpw");
